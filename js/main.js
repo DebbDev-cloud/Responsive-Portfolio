1c57fdcfca1e5d315fe0580700de2cf52ec201e7
DebbDev <82146998+Dhebby-Opeyemi@users.noreply.github.com>
2022-08-30 12:56:35 +0100

    sweet alert

--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
 
 
-const button = document.querySelector("#submit");
-const myName = document.querySelector("#name");
-const email = document.querySelector("#mail");
-const rowIn = document.querySelector("#row");
+ const button = document.querySelector("#submit");
+//  const name = document.querySelector("#name");
+//  const email = document.querySelector("#mail");
+//  const rowIn = document.querySelector("#row");
 
 submit.addEventListener("click", function(){
-   if(myName === "" || email === "" || rowIn === ""){
-    swal({
-        title: "Field empty!!",
-        text: "Please fill the missing field!!",
-        icon: "warning",
-        button: "OK",
-      });
-    } else{
-        swal({
-            title: "Successfully Submitted!",
-            icon: "success",
-            button: "Good job!",
-          })
-    }  
-})
-// function requiredDetails(){
-//     if(myName === ""){
+    
+   const name = document.getElementById("name");
+   const email = document.getElementById("email");
+   const row = document.getElementById("row");
 
-//     }
-// };
+   if( email == "" || name == "" || row == ""){
+    swal({
+        title: "Empty Field!",
+        text: "Please fill the empty field!",
+        icon: "warning",
+        button: "OK!",
+      });
+   } else {
+    swal({
+        title: "Submitted Successfully!",
+        icon: "success",
+        button: "Good Job!",
+      });
+   }
+
+})
+
+// function clickMe(){
+    
+//     swal({
+//         title: "Good job!",
+//         text: "You clicked the button!",
+//         icon: "success",
+//         button: "Aww yiss!",
+//       });
+// }
